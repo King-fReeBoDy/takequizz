c2060ceabc3a834ab3202025d822d98b707734aa
King-fReeBoDy <89941338+King-fReeBoDy@users.noreply.github.com>
2023-04-19 17:21:37 +0000

    first commit

--- src/component/ReviewAnswers.jsx
+++ src/component/ReviewAnswers.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { data } from "./data";
 
 function ReviewAnswers() {
   return (
@@ -7,25 +6,7 @@ function ReviewAnswers() {
       <div>
         <h1>Review answers</h1>
 
-        <div>
-          {data.map((option, index) => {
-            return (
-              <div key={index} className="flex items-center">
-                <input
-                  type="radio"
-                  id={index}
-                  value={option}
-                  name="id"
-                  // disabled
-                  onClick={() => handleAnswer(question, option, options)}
-                />
-                <label htmlFor={index} className="ml-2 text-lg">
-                  {option}
-                </label>
-              </div>
-            );
-          })}
-        </div>
+        <div></div>
       </div>
     </section>
   );
